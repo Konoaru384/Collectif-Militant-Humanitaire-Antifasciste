a421fbbaac56e9053f12409618dccc62ce30d8bc
Konoaru <jordan.jeanson31470@gmail.com>
2026-08-27 22:05:01 +0200

    Remove ally group from allies-config.js
    Removed the group 'Groupe de Léguevin-Brax-Pibrac des jeunes insoumis·es & lycéen·nes' from the ally groups configuration.

--- data/allies-config.js
+++ data/allies-config.js
@@ -1,11 +1,3 @@
 var CMHA_ALLY_GROUPS = [
-    {
-        id: "groupe-de-leguevin-brax-pibrac-des-jeunes-insoumis-es-lyceen-nes",
-        name: "Groupe de Léguevin-Brax-Pibrac des jeunes insoumis·es & lycéen·nes",
-        city: "Léguevin-Brax-Pibrac",
-        lat: 43.5991301,
-        lng: 1.2347784,
-        description: "Groupes LFI qui milite sur le secteur de Brax Leguevin et Pibrac",
-        link: "https://actionpopulaire.fr/groupes/8ce8dce2-99fe-4bd0-9e90-8253bcbed1d5/"
-    }
+
 ];
