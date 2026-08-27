613d73b2338b4e995960631a4098185b8c1ac594
Konoaru <jordan.jeanson31470@gmail.com>
2026-08-27 21:58:57 +0200

    Update allies-config.js

--- data/allies-config.js
+++ data/allies-config.js
@@ -1,11 +1,11 @@
 var CMHA_ALLY_GROUPS = [
     {
-        id: "exemple-allie-1",
-        name: "Nom du groupe allié",
-        city: "Ville",
-        lat: 43.6045,
-        lng: 1.4442,
-        description: "Description du groupe allié et de la nature de la collaboration avec le CMHA.",
-        link: "https://exemple.fr"
+        id: "groupe-de-leguevin-brax-pibrac-des-jeunes-insoumis-es-lyceen-nes",
+        name: "Groupe de Léguevin-Brax-Pibrac des jeunes insoumis·es & lycéen·nes",
+        city: "Léguevin-Brax-Pibrac",
+        lat: 43.5991301,
+        lng: 1.2347784,
+        description: "Groupes LFI qui milite sur le secteur de Brax Leguevin et Pibrac",
+        link: "https://actionpopulaire.fr/groupes/8ce8dce2-99fe-4bd0-9e90-8253bcbed1d5/"
     }
 ];
